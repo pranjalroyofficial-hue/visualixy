c9f652e9938a6ec361b1adbbb39dc071441ab292
Pranjal Roy <pranjalroy.official@gmail.com>
2026-06-22 11:47:54 +0530

    Compiled Visualixy V2 Full Ad-Monetized Framework Matrix and Fixed Layout Bugs

---
+++ config.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://amzrkvnthcheuyifspap.supabase.co';
+const supabaseKey = 'sb_publishable_ksXV7AZ3v_7q02QnGA4_9g_6x5lngVF';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+console.log("⚡ VISUALIXY Central Database Node Connected!");
